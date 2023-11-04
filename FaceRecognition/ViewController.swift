2339ed60c11fa59855bedba6292d863f90f79b42
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-04 13:49:07 +0300

    initial commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by ismail harmanda on 4.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
