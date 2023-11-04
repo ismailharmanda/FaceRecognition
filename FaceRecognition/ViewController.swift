f7290a6090cdf9865af5c4ae4e789e8339f94609
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-04 14:27:45 +0300

    implement face id and updateUI from a background job completion handler

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
     
@@ -20,9 +21,35 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func navigate(){
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
     
     
     @IBAction func onPressSignIn(_ sender: UIButton) {
+        print("test")
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { success, error in
+                
+                if success == true{
+                    DispatchQueue.main.async{
+                        self.navigate()
+                    }
+                } else {
+                    DispatchQueue.main.async{
+                        self.label.text = error?.localizedDescription
+                    }
+                  
+                }
+            }
+        }
     }
     
 
